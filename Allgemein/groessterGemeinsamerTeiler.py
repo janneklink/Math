657dbff50b3e222edad97857902940df3532e013
janneklink <linkjannek@gmail.com>
2020-11-12 15:53:10 +0100

    Zeilennormierung implementiert

---
+++ Allgemein/groessterGemeinsamerTeiler.py
@@ -0,0 +1,7 @@
+def ggt(a,b):
+    while b != 0:
+        q = a // b
+        r = a % b
+        a = b
+        b = r
+    return a
--- LineareGleichungssysteme/gleichungssystem.py
+++ LineareGleichungssysteme/gleichungssystem.py
@@ -1,9 +1,20 @@
+from Allgemein import groessterGemeinsamerTeiler
 class Gleichungssystem:
 
     def __init__(self, matrix):
         self.matrix=matrix
         self.lines = len(matrix)
         self.spalten = len(matrix[0])
+
+    def print(self):
+        distance = 4
+        for line_i in self.matrix:
+            line = ""
+            for element_j_i in line_i:
+                string_element_j = str(int(element_j_i))
+                line += str(string_element_j)
+                line += " " * (distance - len(string_element_j))
+            print(line)
 
     def multipliziere(self, line_i, factor):
         for spalte in range(0, self.spalten):
@@ -26,6 +37,7 @@ class Gleichungssystem:
         self.sortiere_nach_pivoten()
         for spalte_j in range (0, self.spalten):
             self.setze_spalte_null(spalte_j,spalte_j)
+        self.normiere_zeilen()
 
 
     def setze_spalte_null(self,pivot_line, pivot_spalte):
@@ -38,19 +50,31 @@ class Gleichungssystem:
     def sortiere_nach_pivoten(self):
         pass
 
-    def print(self):
-        distance = 4
-        for line_i in self.matrix:
-            line = ""
-            for element_j_i in line_i:
-                string_element_j = str(int(element_j_i))
-                line+= str(string_element_j)
-                line+= " "*(distance-len(string_element_j))
-            print (line)
+
 
     def multipliziere_alle(self, first_line, factor):
         for line_i in range(first_line,self.lines):
             self.multipliziere(line_i, factor)
+
+    def normiere_zeilen(self):
+        for line_i in range (0,self.lines):
+            teiler = self.finde_gemeinsamen_teiler(line_i)
+            self.multipliziere(line_i,1/teiler)
+
+    def finde_gemeinsamen_teiler(self, line_i):
+        teiler = 0
+        for spalte_j in range(1,self.spalten):
+            previous = self.matrix[line_i][spalte_j-1]
+            current = self.matrix[line_i][spalte_j]
+            if teiler==0:
+                teiler=groessterGemeinsamerTeiler.ggt(previous,current)
+            else:
+                teiler=groessterGemeinsamerTeiler.ggt(current,teiler)
+        if teiler!=0:
+            return teiler
+        return 1
+
+
 
 
 
